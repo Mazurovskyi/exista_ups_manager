e22d1952ca6a8fe78e232d23f53b0582db2d282f
vuser <mazurovskiyyevgen@gmail.com>
2023-02-16 18:09:12 +0200

    extended_unworking

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 
-use exista_lib::modbus;
+use exista_lib::{modbus, app};
 use std:: {io, process};
 use std::{result::Result, error::Error, env};
 
@@ -8,22 +8,19 @@ use std::{result::Result, error::Error, env};
 fn main() -> Result<(), Box<dyn Error>>{
 
     let args: Vec<String> = env::args().collect();
+    
+    let path = args.get(1).unwrap_or_default();
 
-    let path = args.get(1).ok_or("Arguments error! Path to port")?;
-    let duration = args.get(2).ok_or("Arguments error! Port timeout (millisec)")?;
-    let timeout:u64 = duration.parse()?;
-
-
-    let port = match modbus::config(path, timeout){
-        Ok(port)=>port,
-        Err(err)=>{println!("ERROR CONFIG MODBUS: {err}"); return Err(err)}
+    //configure the application
+    let app_entities = match app::config(path){
+        Ok(app_entities) => app_entities,
+        Err(err) => return Err(format!("CONFIGURATION ERROR: {err}").into())
     };
 
-
-    if let Err(err) = modbus::run(port){
-        println!("ERROR RUNNING MODBUS: {err}");
-        return Err(err)
-    };
+    //run the application
+    if let Err(err) = app::run(app_entities){
+        return Err(format!("RUN ERROR: {err}").into())
+    }
     
     Ok(())
 }
